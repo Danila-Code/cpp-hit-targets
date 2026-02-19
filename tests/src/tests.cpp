57c6f982f76d42074032abd1bbcb715e93d5dc9c
Danila-Code <danila.Zcode@gmail.com>
2026-02-19 16:33:04 +0300

    fix height/width calc add middle/edge points tests

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -34,17 +34,44 @@ TEST(TestQuadTree, NoTargetsFound) {
     EXPECT_EQ(q_tree.getCount(Circle({70, 70}, 5)), 0);
 }
 
+TEST(TestQuadTree, FindTargetsOnEdge) {
+    std::vector<Point> points = {{0, 0}, {0, 99}, {99, 0}, {99, 99}};
+    Rectangle rect{{0, 0}, {99, 99}};
+
+    QuadTree q_tree(rect);
+    // заполнение дерева точками
+    for (const auto& p : points) {
+        q_tree.insert(p);
+    }
+    // проверка результата
+    EXPECT_EQ(q_tree.getCount(Circle({1, 1}, 5)), 1);
+    EXPECT_EQ(q_tree.getCount(Circle({1, 98}, 5)), 1);
+    EXPECT_EQ(q_tree.getCount(Circle({98, 1}, 5)), 1);
+    EXPECT_EQ(q_tree.getCount(Circle({98, 98}, 5)), 1);
+}
+
+TEST(TestQuadTree, FindTargetsOnTheMiddle) {
+    std::vector<Point> points = {{50, 50}, {50, 50}};
+    Rectangle rect{{0, 0}, {99, 99}};
+
+    QuadTree q_tree(rect);
+    // заполнение дерева точками
+    for (const auto& p : points) {
+        q_tree.insert(p);
+    }
+    // проверка результата
+    EXPECT_EQ(q_tree.getCount(Circle({49, 49}, 5)), 2);
+}
+
 TEST(TestQuadTree, FindTragets) {
     std::vector<Point> points = {{5, 5}, {5, 6}, {16, 2}, {17, 6}, {40, 40}, {43, 35}, {45, 45}};
     Rectangle rect{{0, 0}, {99, 99}};
 
     QuadTree q_tree(rect);
-
     // заполнение дерева точками
     for (const auto& p : points) {
         q_tree.insert(p);
     }
-
     // проверка результата
     EXPECT_EQ(q_tree.getCount(Circle({5, 5}, 5)), 2);
     EXPECT_EQ(q_tree.getCount(Circle({15, 4}, 5)), 2);
